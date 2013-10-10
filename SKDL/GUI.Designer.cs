2aa033d3190eacd08b0ec2d93f6b0eda4ecdc787
Mikael Hernrup <mikael@hernrup.se>
2013-10-10 13:33:41 +0200

    GUI

---
+++ SKDL/GUI.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace SKDL
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelContent
+            // 
+            this.panelContent.Location = new System.Drawing.Point(12, 12);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(260, 237);
+            this.panelContent.TabIndex = 0;
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panelContent);
+            this.Name = "GUI";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContent;
+    }
+}
+
---
+++ SKDL/GUI.cs
@@ -0,0 +1,63 @@
+﻿using SKDLViewControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SKDL
+{
+    public partial class GUI : Form
+    {
+        public GUI()
+        {
+            InitializeComponent();
+            FormView = FormViews.A; //simply change views like this
+        }
+
+        public enum FormViews {
+            A, B
+        }
+        private ViewA viewA; //user control with view a on it 
+        private ViewB viewB; //user control with view b on it
+
+        private FormViews _formView;
+        public FormViews FormView {
+            get {
+                return _formView;
+            }
+            set {
+                _formView = value;
+                OnFormViewChanged(_formView);
+            }
+        }
+        protected virtual void OnFormViewChanged(FormViews view) {
+            //contentPanel is just a System.Windows.Forms.Panel docked to fill the form
+            switch (view) {
+                case FormViews.A:
+                    if (viewA == null) viewA = new ViewA();
+                    //extension method, you could use a static function.
+                    this.panelContent.DockControl(viewA);
+                    break;
+                case FormViews.B:
+                    if (viewB == null) viewB = new ViewB();
+                    this.panelContent.DockControl(viewB);
+                    break;
+            }
+        }
+    }
+
+    public static class PanelExtensions {
+        public static void DockControl(this Panel thisControl, Control controlToDock) {
+            thisControl.Controls.Clear();
+            thisControl.Controls.Add(controlToDock);
+            controlToDock.Dock = DockStyle.Fill;
+            //controlToDock.Visible = true;
+        }
+    }
+}
